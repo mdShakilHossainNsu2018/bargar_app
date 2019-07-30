34de81e3657652be87dbb4e863d805c7701f6dfc
mdShakilHossainNsu20!8 <shakilnsu2018@gmail.com>
2019-07-30 21:38:18 +0600

    commit for deploy to heroku

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,24 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+
+    let transformedBurger = Object.keys(props.ingredients).map(igKey=>{
+
+        return [...Array(props.ingredients[igKey])].map((_,i)=>{
+            return <BurgerIngredient key={igKey+1} type={igKey}/>
+        })
+    }).reduce((arr, el)=>{
+        return arr.concat(el);
+    });
+
+    if (transformedBurger.length === 0){
+        transformedBurger = <p>Please add ingredient</p>
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type={'bread-top'}/>
-            <BurgerIngredient type={'meat'}/>
-            <BurgerIngredient type={'bacon'}/>
+            {transformedBurger}
             <BurgerIngredient type={'bread-bottom'}/>
         </div>
     )
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,23 @@
 import React, {Fragment} from 'react';
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
 export default class BurgerBuilder extends React.Component{
+
+    state = {
+      ingredients: {
+          salad:0,
+          bacon:0,
+          cheese:0,
+          meat:0,
+      }
+    };
 
     render() {
         return(
             <Fragment>
-               <Burger/>
-                <div>Build container</div>
+               <Burger ingredients={this.state.ingredients}/>
+                <BuildControls/>
             </Fragment>
         );
     }
